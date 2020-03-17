e34b2843b67b549694a8f68c183f49e49bd7f454
dreamfly <fujia0@qq.com>
2020-03-18 07:34:00 +0800

    jfu

--- app/database.php
+++ app/database.php
@@ -15,9 +15,9 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'mi.cn',
+    'database'        => 'mi2.cn',
     // 用户名
-    'username'        => 'xiaomi',
+    'username'        => 'mi2',
     // 密码
     'password'        => '123456',
     // 端口
